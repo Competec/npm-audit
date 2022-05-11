2f7636f0ce1857067cc416de9d2a6676ceac91ab
Francis <francis0006@yahoo.de>
2022-05-11 08:13:18 +0200

    Clean up code

--- src/utils/cleanup-audit.js
+++ src/utils/cleanup-audit.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const config = require('../config');
+const logger = require('./logger');
 
 module.exports = ({tmpDir}) => {
     try {
@@ -7,8 +8,8 @@ module.exports = ({tmpDir}) => {
             fs.rmSync(tmpDir, {recursive: true});
         }
 
-        console.log(`Your audit summary has been created. See file ${config.REPORT_STATS_FILE}`);
+        logger.info(`Your audit summary has been created. See file ${config.REPORT_STATS_FILE}`);
     } catch (e) {
-        console.error(`An error has occurred while removing the temp folder at ${tmpDir}. Please remove it manually. Error: ${e}`);
+        logger.error(`An error has occurred while removing the temp folder at ${tmpDir}. Please remove it manually. Error: ${e}`);
     }
 };
